1bfae23a54adc2d014cb774a2d6a585725a70ece
Alex Rosen <ar548@njit.edu>
2015-01-21 23:48:34 -0500

    set up the acceptance of cmd line args

--- project_1/funnyMemo.cc
+++ project_1/funnyMemo.cc
@@ -3,7 +3,13 @@
 
 using namespace std;
 
-int main(){
-
-  return 0;
+int main (int argc, char *argv[]) {
+	//argc is the number of cmd arguments argv is an array of the arguments
+	//where argv[0] is the first going from left to right
+	cout << "There were " << argc << " arguments on the command line" << endl;
+	for( int i=0; i<argc; i++ ) {
+		cout << i << ":" << argv[i] << ":" << endl;
+	}
+	
+    return 0;
 }
