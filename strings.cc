84428d62fa97890461d6556fd28d14940e24d716
Alex Rosen <ar548@njit.edu>
2015-01-26 13:40:49 -0500

    stuff from class

---
+++ strings.cc
@@ -0,0 +1,8 @@
+#include<string>
+#include<iostream>
+
+using namespace std;
+
+// a string in C is a string of characters with a NULL characters at the end
+// this is simmilar to an array of characters
+
