30869621cdc3b63fdde34fd3b70b963c741d83d6
Alex Rosen <ar548@njit.edu>
2015-01-26 13:49:29 -0500

    stuff from class

--- strings.cc
+++ strings.cc
@@ -6,3 +6,11 @@ using namespace std;
 // a string in C is a string of characters with a NULL characters at the end
 // this is simmilar to an array of characters
 
+int main (){
+	string mystring = "hello!";
+	string yourstring = mystring;
+
+	cout << mystring.length() << endl;
+	
+    return 0;
+}
