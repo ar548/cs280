f58b25899af7a22aca6c256ae688078784715335
Alex Rosen <ar548@njit.edu>
2015-03-14 09:43:55 -0400

    setup for project 3

---
+++ project_3/main.cpp
@@ -0,0 +1,14 @@
+#include "p3lex.h"
+#include <iostream>
+
+extern program();
+extern stmtList();
+extern stmt();
+extern expr();
+extern term();
+extern primary();
+
+int main(int argc, char* argv[]){
+	
+return 0;
+}
