611f4842ad6d26bea5b00bb6ad3aeaa5fcb037f6
Alex Rosen <ar548@njit.edu>
2015-03-25 16:15:07 -0400

    I have a few questions but i am now making far more progress

--- project_3/main.cpp
+++ project_3/main.cpp
@@ -1,20 +1,113 @@
 #include "p2lex.h"
-#include "classes.h"
 #include <iostream>
-#include <vector>
 #include <fstream>
+#include <string>
+#include <map>
 
-using namespace std;
+// global variables for the file
+map<string, int> setIDs;
 
-int main(int argc, char* argv[]){
+class PTree {
+	PTree *left;
+	PTree *right;
 
+public:
+	PTree(PTree *left, PTree *right = 0) {
+		this->left = left;
+		this->right = right;
+	}
+};
+
+class PTreeStmtList : public PTree {
+public:
+	PTreeStmtList(PTree *s1, PTree *s2 = 0) : PTree(s1, s2) {};
+};
+
+class PTreeSet : public PTree {
+public:
+	PTreeSet(PTree *s1, PTree *s2 = 0) : PTree(s1, s2) {};
+};
+
+class PTreePrint : public PTree {
+public:
+	PTreePrint(PTree *s) : PTree(s, 0) {};
+};
+
+extern PTree *Program(istream *br);
+extern PTree *StmtList(istream *br);
+extern PTree *Stmt(istream *br);
+extern PTree *Expr(istream *br);
+extern PTree *Term(istream *br);
+extern PTree *Primary(istream *br);
+
+// Program ::= StmtList
+PTree *Program(istream *br){
+	return StmtList(br);
+}
+
+// StmtList ::= Stmt | Stmt StmtList
+PTree *StmtList(istream *br){
+	PTree *stmt;
+
+	stmt = Stmt(br);
+
+	if( stmt ){
+		return new PTreeStmtList( stmt, StmtList(br) );
+	}
+	return 0;
+}
+
+// Stmt ::= PRINT Expr SC | SET ID Expr SC
+PTree *Stmt(istream *br){
+	PTree *stmt;
+	string lex;
+
+	Token T = getToken(br, lex);
+	if( T == PRINT ){
+		return new PTreePrint( Expr(br) );	
+	}
+	else if( T == SET ){
+		Token T2 = getToken(br, lex);
+		if( T2 != ID ){
+			cerr << "A set statement must follow the pattern \"SET ID Expr;\"" << endl;
+			return 0;
+		}
+		else{
+			setIDs[lex] = 1;
+			return new PTreeSet( lex, Expr(br) );	// TODO this line needs to be fixed 
+								// ask the prof how to make just the ID
+								// a PTree leaf
+		}	
+	}
+	else{
+		cerr << "This is an invalid statement" << endl;
+		cerr << "A statement is either \"PRINT Expr;\" or \"SET ID Expr;\"" << endl;
+		// TODO ask the prof what needs to be done in the case of a non conforming statement
+		return 0;
+	}
+}
+
+// Expr ::= Expr PLUS Term | Expr MINUS Term | Term
+PTree *Expr(istream *br){
+	return 0;
+}
+
+PTree *Term(istream *br){
+	return 0;
+}
+
+PTree *Primary(istream *br){
+	return 0;
+}
+
+int main(int argc, char *argv[]){
 	istream *br;
 	ifstream infile;
 	string lex;
-	
+				
 	if(argc == 2){
-		//gets the input from a file
-		infile.open(argv[1]);
+		// gets the input from a file
+		infile.open( argv[1] );
 		if( infile.is_open() ){
 			br = &infile;
 		}
@@ -23,8 +116,8 @@ int main(int argc, char* argv[]){
 			return 1;
 		}
 	}
-	else if(argc == 1){
-		//gets standard user input
+	else if( argc == 1 ){
+		// gets standard user input
 		br = &cin;
 	}
 	else{
@@ -34,7 +127,5 @@ int main(int argc, char* argv[]){
 		return 1;
 	}
 
-	program prg(br, lex);
-
-return 0;
+	PTree *prg = Program(br);
 }
